b56c2a76ba1cb629ca6b44e94ded8399b253f77e
Jamie Ho <jamie.ho@airbnb.com>
2016-04-23 17:07:22 -0700

    Adding edit activity and moving storage code to separate class.

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.codepath.simpletodo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,30 +11,43 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.io.File;
-import java.io.IOException;
+import com.codepath.simpletodo.storage.SimpleTodoStorage;
+import com.codepath.simpletodo.storage.impl.FileSimpleTodoStorage;
+
 import java.util.ArrayList;
 
-import org.apache.commons.io.FileUtils;
-
+/**
+ * The main activity of creating, listing and deleting todo items.
+ */
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
-    ListView lvItems;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private final int REQUEST_CODE = 20;
+    private SimpleTodoStorage storage;
 
+    /**
+     * Initialize the MainActivity and read the saved to do list.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvlItems);
-        readItems();
+        storage = new FileSimpleTodoStorage(getFilesDir());
+        items = storage.readItems();
         itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
     }
 
+    /**
+     * Set up the click and long click listeners for the todo list.
+     */
     private void setupListViewListener() {
+        //Delete the item that is long clicked.
         lvItems.setOnItemLongClickListener(
                 new AdapterView.OnItemLongClickListener() {
                     @Override
@@ -41,8 +55,21 @@ public class MainActivity extends AppCompatActivity {
                         View item, int pos, long id) {
                         items.remove(pos);
                         itemsAdapter.notifyDataSetChanged();
-                        writeItems();
+                        storage.writeItems(items);
                         return true;
+                    }
+                });
+
+        //Edit the item that is long clicked.
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                        i.putExtra("item", items.get(pos));
+                        i.putExtra("pos", pos);
+                        startActivityForResult(i, REQUEST_CODE);
                     }
                 });
     }
@@ -63,31 +90,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Create a new todo item.
+     */
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
-        writeItems();
+        storage.writeItems(items);
     }
 
-    public void readItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            items = new ArrayList<>(FileUtils.readLines(todoFile));
-        } catch (IOException e) {
-            items = new ArrayList<>();
-        }
-    }
-
-    public void writeItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            FileUtils.writeLines(todoFile, items);
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * Save the edited todo item.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            String item = data.getExtras().getString("item");
+            int pos = data.getExtras().getInt("pos", 0);
+            items.set(pos, item);
+            itemsAdapter.notifyDataSetChanged();
+            storage.writeItems(items);
         }
     }
 }
