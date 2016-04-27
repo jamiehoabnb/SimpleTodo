49dd13de84ee89f569a33f211786ea3c7be63498
Jamie Ho <jamie.ho@airbnb.com>
2016-04-27 15:46:24 -0700

    Increase size of create/edit fragment.

--- app/src/main/java/com/codepath/simpletodo/fragments/CreateEditToDoItemDialogFragment.java
+++ app/src/main/java/com/codepath/simpletodo/fragments/CreateEditToDoItemDialogFragment.java
@@ -145,7 +145,7 @@ public class CreateEditToDoItemDialogFragment extends DialogFragment implements 
         Point size = new Point();
         Display display = window.getWindowManager().getDefaultDisplay();
         display.getSize(size);
-        window.setLayout((int) (size.x), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setLayout((int) (size.x), (int) (size.y));
         window.setGravity(Gravity.CENTER);
         super.onResume();
     }
